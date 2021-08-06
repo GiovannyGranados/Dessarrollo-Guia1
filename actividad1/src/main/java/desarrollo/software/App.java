41ad01bc432c3e5d306706798e7f159a9d6134ed
GiovannyGranados <gags_07@hotmail.com>
2021-08-06 18:35:40 -0500

    Ejemplo juan VSCode

--- actividad1/src/main/java/desarrollo/software/App.java
+++ actividad1/src/main/java/desarrollo/software/App.java
@@ -11,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Guia 1 - Actividad 1" );
+        System.out.print("Esta es una modificacion voluntaria");
     }
 }
